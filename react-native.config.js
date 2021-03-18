b857137d550f1d84566bed8af7ca3d7ecfbeb0d3
Matthew Harrison <matthewharrison@Matthews-MBP-2.attlocal.net>
2021-03-18 01:46:07 -0400

    created assets folder for fonts, images and icons; constants folder for theme,icons, images; screens folder for home, scan and signup; react-native.config file for fonts on ios and android

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {}
+    },
+    assets: ['./assets/fonts/']
+}
+//we need to install fonts in respective prjects on ios and android
---
+++ screens/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native';
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>Sign Up</Text>
+        </View>
+    )
+};
+
+export default SignUp;
